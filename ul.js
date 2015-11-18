76c80898168c7e872242d5561d744aaff9feeb3d
Emanuele Brivio <emanuele.brivio@outlook.com>
2015-11-18 23:55:54 +0100

    New behaviour with module export

--- ul.js
+++ ul.js
@@ -1,27 +1,35 @@
-// export to window.ul the first language set in browser
-window.ul = function () {
+!function (name, context, definition) {
+  if (typeof module != 'undefined' && module.exports) module.exports = definition()
+  else if (typeof define == 'function' && define.amd) define(definition)
+  else context[name] = definition()
+}('ul', this, function () {
+  
   var nav = window.navigator
   var browserKeys = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage']
   var i
   var lng;
+  
+  function ul () {
+    // support for HTML 5.1 "navigator.languages"
+    if (Array.isArray(nav.languages)) {
+      for (i = 0; i < nav.languages.length; i++) {
+        lng = nav.languages[i];
+        if (lng && lng.length) {
+          return lng;
+        }
+      }
+    }
 
-  // support for HTML 5.1 "navigator.languages"
-  if (Array.isArray(nav.languages)) {
-    for (i = 0; i < nav.languages.length; i++) {
-      lng = nav.languages[i];
+    // support for other well known properties in browsers
+    for (i = 0; i < browserKeys.length; i++) {
+      lng = nav[browserKeys[i]];
       if (lng && lng.length) {
         return lng;
       }
     }
-  }
 
-  // support for other well known properties in browsers
-  for (i = 0; i < browserKeys.length; i++) {
-    lng = nav[browserKeys[i]];
-    if (lng && lng.length) {
-      return lng;
-    }
+    return null;
   }
-
-  return null;
-}();
+  
+  return ul;
+});
